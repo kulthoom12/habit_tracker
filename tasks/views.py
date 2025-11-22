afa22d664cfafbc194545c4a710190f317b9e181
kulthoom.12 <mussah.kulsum@gmail.com>
2025-11-22 23:42:03 +0000

    Correct minor errors

--- tasks/views.py
+++ tasks/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect
 from .models import Task
 
+
 def home(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         if title:
-            Task.objects.create(title=title, user=request.user if request.user.is_authenticated else None)
+            Task.objects.create(
+                title=title, user=request.user if request.user.is_authenticated else None)
         return redirect('home')
 
         if request.user.is_authenticated:
             tasks = Task.objects.filter(user=request.user)
 
-            else:            
+            else:
+
     tasks = Task.objects.all()
-    
+
     return render(request, 'tasks/home.html', {'tasks': tasks})

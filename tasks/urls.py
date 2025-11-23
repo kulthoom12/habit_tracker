ca40423a3fc13b7cd0645cb0c72b77c4ba5c719b
kulthoom.12 <mussah.kulsum@gmail.com>
2025-11-23 01:01:05 +0000

    Format documents

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path( '', views.task_lis, name='task_list')
-  path('task/create/',view.task_create, name='task_create'),
-  path('task/update<int:pk>/', views.task_update, name='task_update'),
-  path('task/delet/<int:pk>/', views.task_delete, name='task_delete'),
-  path('task/clear_completed/', views.clear_complted, name='clear_completed'),
+    path('', views.task_list, name='task_list'),
+    path('task/create/', views.task_create, name='task_create'),
+    path('task/update<int:pk>/', views.task_update, name='task_update'),
+    path('task/delete/<int:pk>/', views.task_delete, name='task_delete'),
+    path('task/clear_completed/', views.clear_completed, name='clear_completed'),
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -7,14 +7,14 @@ def home(request):
         title = request.POST.get('title')
         if title:
             Task.objects.create(
-                title=title, user=request.user if request.user.is_authenticated else None)
+                title=title,
+                user=request.user if request.user.is_authenticated else None
+            )
         return redirect('home')
 
-        if request.user.is_authenticated:
-            tasks = Task.objects.filter(user=request.user)
-
-            else:
-
-    tasks = Task.objects.all()
+    if request.user.is_authenticated:
+        tasks = Task.objects.filter(user=request.user)
+    else:
+        tasks = Task.objects.all()
 
     return render(request, 'tasks/home.html', {'tasks': tasks})

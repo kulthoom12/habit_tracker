e9aa1111a3f91becd5f5b529143cb6f03f870cc0
kulthoom.12 <mussah.kulsum@gmail.com>
2025-11-24 21:51:28 +0000

    fix urls for update and delete tasks.

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('task/create/', views.task_create, name='task_create'),
-    path('task/update<int:pk>/', views.task_update, name='task_update'),
+    path('task/update/<int:pk>/', views.task_update, name='task_update'),
     path('task/delete/<int:pk>/', views.task_delete, name='task_delete'),
     path('task/clear_completed/', views.clear_completed, name='clear_completed'),
 

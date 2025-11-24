91478d91d2341cdc6b7d42ca80a0d5bc5b78eed4
kulthoom.12 <mussah.kulsum@gmail.com>
2025-11-24 22:03:23 +0000

    Fxed url, use task_list not home.

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.task_list, name='task_list'),
+    path('', views.task_list, name='home'),
     path('task/create/', views.task_create, name='task_create'),
     path('task/update/<int:pk>/', views.task_update, name='task_update'),
     path('task/delete/<int:pk>/', views.task_delete, name='task_delete'),

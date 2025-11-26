577974616d44ad4f46e879c19ad476f4be545694
kulthoom.12 <mussah.kulsum@gmail.com>
2025-11-26 20:15:38 +0000

    Update urls for habits pages

--- habits/urls.py
+++ habits/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('habits/create/', views.habits_create, name='habits_create'),
     path('habits/update/<int:pk>/', views.habits_update, name='habits_update'),
     path('habits/delete/<int:pk>/', views.habits_delete, name='habits_delete'),
-    path('habits/clear_completed/', views.clear_completed, name='habits_clear_completed'),
+    path('habits/clear_completed/', views.habits_clear_completed, name='habits_clear_completed'),
 
     path('login/', views.login_view, name='login'), 
     path('register/', views.register, name='register'),

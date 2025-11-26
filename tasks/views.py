d31bc4c94d6d11f84e1d0745813b97f438017da6
kulthoom.12 <mussah.kulsum@gmail.com>
2025-11-26 01:01:37 +0000

    Add login view and protect task views with auth

--- tasks/views.py
+++ tasks/views.py
@@ -1,26 +1,41 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 from .models import Task
 
-def task_list(request):
-    if request.user.is_authenticated:
-        tasks = Task.objects.filter(user=request.user)
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)  
+        if form.is_valid():
+            user = form.get_user() 
+            login(request, user)    
+            return redirect('home')  
     else:
-        tasks = Task.objects.all()
+        form = AuthenticationForm()  
+    
+    return render(request, "tasks/login.html", {"form": form})  
 
+def task_list(request):
+    if not request.user.is_authenticated:  
+        return redirect('login')  
+
+    tasks = Task.objects.filter(user=request.user)  
     return render(request, "tasks/index.html", {"tasks": tasks})
 
-
 def task_create(request):
+    if not request.user.is_authenticated:  
+        return redirect('login')  
+
     if request.method == "POST":
         title = request.POST.get("title")
         if title != "":
             Task.objects.create(
-                user=request.user if request.user.is_authenticated else None,
+                user=request.user,  
                 title=title
             )
-        return redirect("task_list")
-    return render(request, "tasks/form.html")
+        return redirect("task_list")  
 
+    return render(request, "tasks/form.html")  
 
 def task_update(request, pk):
     task = get_object_or_404(Task, id=pk)

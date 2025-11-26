fec4ed866b1e495183f4f89bb6a82e9b35fe7349
kulthoom.12 <mussah.kulsum@gmail.com>
2025-11-26 05:40:59 +0000

    Change name

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.habit_list, name='habit_list'), 
+    path('', views.habit_list, name='habit_list'),
     path('habit/create/', views.habit_create, name='habit_create'),
     path('habit/update/<int:pk>/', views.habit_update, name='habit_update'),
     path('habit/delete/<int:pk>/', views.habit_delete, name='habit_delete'),

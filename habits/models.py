4efc67964f1948c8da5c8cfb49513e27ae42eaef
kulthoom.12 <mussah.kulsum@gmail.com>
2025-11-27 03:40:48 +0000

    Fix typo in habit model

--- habits/models.py
+++ habits/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Habit(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
     completed_today = models.BooleanField(default=False)
-    frequency = models.CharField(max_length=10)  
+    frequency = models.CharField(max_length=10)
     streak = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
 

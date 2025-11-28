0a8596463cf0e320c429bdd6aa996eab6c7c1a10
kulthoom.12 <mussah.kulsum@gmail.com>
2025-11-28 18:04:43 +0000

    Change habit_list to home

--- habits/urls.py
+++ habits/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     
     # Habit Views
-    path('', views.habits_list, name='home'),  # Set this as the home page
+    path('', views.home, name='home'),
     path('habits/add/', views.habit_form, name='habits_create'),
     path('habits/edit/<int:pk>/', views.habit_form, name='habits_update'),
     path('habits/delete/<int:pk>/', views.habits_delete, name='habits_delete'),

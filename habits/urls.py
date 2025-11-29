6924e429c7187d7e0a6ca254ca3660b11e5d3db1
kulthoom.12 <mussah.kulsum@gmail.com>
2025-11-29 23:52:18 +0000

    fix login page for redirection

--- habits/urls.py
+++ habits/urls.py
@@ -3,11 +3,13 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+  path('', views.login_view, name='login'),
+  
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 
-    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
 
     path('habits/add/', views.habit_form, name='habits_create'),
     path('habits/edit/<int:pk>/', views.habit_form, name='habits_update'),
--- habits/views.py
+++ habits/views.py
@@ -6,19 +6,28 @@ from django.contrib import messages
 from .forms import HabitForm
 from .models import Habit
 
+def redirect_to_login(request):  
+    if request.user.is_authenticated:  
+        return redirect('home') 
+    return redirect('login')  
+
+
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            messages.success(request, "Registration successful! You are now logged in.")
+            messages.success(
+                request, "Registration successful! You are now logged in.")
             return redirect('home')
         else:
-            messages.error(request, "There was an error with your registration.")
+            messages.error(
+                request, "There was an error with your registration.")
     else:
         form = UserCreationForm()
     return render(request, "habits/register.html", {"form": form})
+
 
 def login_view(request):
     if request.method == "POST":
@@ -34,8 +43,12 @@ def login_view(request):
         form = AuthenticationForm()
     return render(request, "habits/login.html", {"form": form})
 
+
+#  login_required
 @login_required
 def home(request):
+    if not request.user.is_authenticated:
+          return redirect('login')
     habits = Habit.objects.filter(user=request.user).order_by('-id')
     return render(request, "habits/index.html", {"habits": habits})
 
@@ -61,12 +74,14 @@ def habit_form(request, pk=None):
 
     return render(request, "habits/form.html", {"form": form, "habit": habit})
 
+
 @login_required
 def habits_delete(request, pk):
     habit = get_object_or_404(Habit, id=pk, user=request.user)
     habit.delete()
     messages.success(request, "Habit deleted successfully.")
     return redirect("home")
+
 
 @login_required
 def habits_clear_completed(request):

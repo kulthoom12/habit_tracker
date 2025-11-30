7b8c00472fdf6bbe9668cce33cee27da33e070a7
kulthoom.12 <mussah.kulsum@gmail.com>
2025-11-30 00:35:10 +0000

    debug navbar for functionality

--- habits/urls.py
+++ habits/urls.py
@@ -1,18 +1,24 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
+
+
+def redirect_to_login(request):
+    return redirect('login')
+
 
 urlpatterns = [
-  path('', views.login_view, name='login'),
-  
+    path('', redirect_to_login, name='root'),
+    path('home/', views.home, name='home'),
+
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 
-    path('home', views.home, name='home'),
-
     path('habits/add/', views.habit_form, name='habits_create'),
     path('habits/edit/<int:pk>/', views.habit_form, name='habits_update'),
     path('habits/delete/<int:pk>/', views.habits_delete, name='habits_delete'),
-    path('habits/clear_completed/', views.habits_clear_completed, name='habits_clear_completed'),
+    path('habits/clear_completed/', views.habits_clear_completed,
+         name='habits_clear_completed'),
 ]
